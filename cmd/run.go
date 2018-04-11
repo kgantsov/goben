c115eb47c633ce53e5dd9db8fbd9b2dfe3f337c7
Kostiantyn Hantsov <k.gantsov@gmail.com>
2018-04-11 21:07:38 +0200

    Add blank cmd package

---
+++ cmd/run.go
@@ -0,0 +1,60 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// runCmd represents the run command
+var runCmd = &cobra.Command{
+	Use:   "run",
+	Short: "Run benchmark",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		url, err := cmd.Flags().GetString("url")
+		if err != nil {
+			return err
+		}
+		connections, err := cmd.Flags().GetUint16("connections")
+		if err != nil {
+			return err
+		}
+		requests, err := cmd.Flags().GetUint32("requests")
+		if err != nil {
+			return err
+		}
+		fmt.Println(">>>>", url, connections, requests, err)
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(runCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// runCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	runCmd.Flags().Uint32P("requests", "n", 100000, "Number of requests")
+	runCmd.Flags().Uint16P("connections", "c", 100, "Number of connections")
+	runCmd.Flags().StringP("url", "u", "", "URL that will be benchmarked")
+}
